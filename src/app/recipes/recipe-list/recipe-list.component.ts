8436da26d79146e7773179cb9c915c265d8cd04e
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-04-02 13:57:53 +0530

    recipe-detail component is added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {Recipe} from "../recipe";
 import {RecipeItemComponent} from "./recipe-item.component";
 
@@ -11,6 +11,8 @@ export class RecipeListComponent implements OnInit {
 
   recipes: Recipe[] = [];
 
+  @Output() recipeSelected = new EventEmitter<Recipe>();
+
   recipe = new Recipe('Dummy','Dummy','https://thumb7.shutterstock.com/display_pic_with_logo/2312339/398527027/stock-photo-full-length-female-mannequin-dressed-in-yellow-dress-isolated-on-white-background-398527027.jpg');
 
   constructor() { }
@@ -18,4 +20,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit() {
   }
 
+  onSelected(recipe: Recipe){
+    this.recipeSelected.emit(recipe);
+  }
 }
--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {RecipeListComponent} from "./recipe-list/recipe-list.component";
+import {RecipeDetailComponent} from "./recipe-detail/recipe-detail.component";
+import {Recipe} from "./recipe";
 
 @Component({
   selector: 'rb-recipes',
   templateUrl: './recipes.component.html',
-  entryComponents:[RecipeListComponent]
+  entryComponents:[RecipeListComponent, RecipeDetailComponent]
 })
 export class RecipesComponent implements OnInit {
+
+  selectedRecipe:Recipe;
 
   constructor() { }
 

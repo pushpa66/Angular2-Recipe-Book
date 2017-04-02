d5f40275c2176bbf6853df27526ce356449961f9
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-04-02 11:31:16 +0530

    header component is added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./header.component";
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  entryComponents: [HeaderComponent]
 })
 export class AppComponent {
-  title = 'rb works!';
+
 }

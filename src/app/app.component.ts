ea52504f23168844e542a2577f3079323d5dc26a
pushpe <pushpakumara.14@cse.mrt.ac.lk>
2017-04-02 14:23:50 +0530

    shopping-list and shopping-list-add components are added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from "./header.component";
 import {RecipesComponent} from "./recipes/recipes.component";
+import {ShoppingListComponent} from "./recipes/shopping-list/shopping-list.component";
 
 @Component({
   selector: 'rb-root',
   templateUrl: './app.component.html',
-  entryComponents: [HeaderComponent,RecipesComponent]
+  entryComponents: [HeaderComponent,RecipesComponent,ShoppingListComponent]
 })
 export class AppComponent {
 
